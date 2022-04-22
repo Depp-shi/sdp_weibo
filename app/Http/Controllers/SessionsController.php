3ff88e0a1778aa3139e75d05a16d569f7fb73fc9
Depp-shi <946707789@qq.com>
2022-04-22 16:26:12 +0800

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -10,6 +10,7 @@ class SessionsController extends Controller
     {
         return view('sessions.create');
     }
+
     public function store(Request $request)
     {
        $credentials = $this->validate($request, [
@@ -24,5 +25,12 @@ class SessionsController extends Controller
             return redirect()->back()->withInput();
         }
        return;
+    }
+
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect('login');
     }
 }
